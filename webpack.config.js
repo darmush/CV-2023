8165c60795fdf91ec2d0b200cf78a629719623fa
Darya Podskrebalina <2012dr2012@gmail.com>
2023-03-17 13:22:30 +0600

    feat: install webpack and tailwind

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+	devServer: {
+      port: 3000,
+      open: true,
+      hot: true,
+  },
+  entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: 'index.[contenthash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          sourceType: 'unambiguous',
+        },
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
